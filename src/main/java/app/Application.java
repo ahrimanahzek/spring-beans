eeb5a011b46f4caf8d633d8215adfb7646f738ac
Djabrail <jabrail8@mail.ru>
2020-12-12 22:31:44 +0300

    1

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,9 @@
 package app;
 
-import app.config.AppConfig;
-import app.model.AnimalsCage;
+import app.config.*;
+import app.model.*;
+/*import main.java.app.config.AppConfig;
+import app.model.AnimalsCage;*/
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,9 +12,9 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = (AnimalsCage) applicationContext.getBean("animalsCage");
             bean.whatAnimalSay();
         }
     }
